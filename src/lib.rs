7d05cf34fac2cfd5ce001823886eef0e56f00213
Antoine Marneur <antoinemarneur@antoines-macbook-air.home>
2022-07-14 21:33:57 +0200

    clean + const variables + use of 2 dimensions vector (Vec2)

--- src/lib.rs
+++ src/lib.rs
@@ -1,112 +1,111 @@
 use nannou::prelude::*;
 
+const WIDTH: f32 = 15.0;
+const HEIGHT: f32 = 15.0;
+const MARGIN: f32 = 100.0;
+const SPEED_LIMIT: f32 = 10.0;
+const SEPARATION: f32 = 40.0;
+const DISTANCE: f32 = 100.0;
+
+
 #[derive(PartialEq)]
 pub struct Boid {
-    x: f32,
-    y: f32,
-    dx: f32,
-    dy: f32,
+    position: Vec2,
+    velocity: Vec2,
 }
 
 impl Boid {
-    pub fn new(x: f32, y: f32, velocity: f32) -> Boid {
+    pub fn new(position: Vec2, velocity: Vec2) -> Boid {
         Boid {
-            x,
-            y,
-            dx: velocity,
-            dy: velocity,
+            position,
+            velocity,
         }
     }
 
     pub fn draw(&self, draw: &Draw) {
         draw.tri().color(BLACK)
-            .w_h(15.0, 15.0)
-            .x_y(self.x, self.y)
-            .rotate(vec2(self.dx, self.dy).angle());
+            .w_h(WIDTH, HEIGHT)
+            .xy(self.position)
+            .rotate(self.velocity.angle());
     }
 
-    pub fn update(&mut self, x: f32, y: f32) {
-        self.dx += x;
-        self.dy += y;
+    pub fn update(&mut self, velocity: Vec2) {
+        self.velocity += velocity;
 
-        self.speedLimit();
+        self.speed_limit();
 
-        self.x += self.dx;
-        self.y += self.dy;
+        self.position += self.velocity;
     }
 
     // Regulate the speed of every boid to behave boids like real animals.
-    fn speedLimit(&mut self) {
-        let speedLimit = 10.0;
+    fn speed_limit(&mut self) {
+        let speed_limit = SPEED_LIMIT;
 
-        let speed = (self.dx * self.dx + self.dy * self.dy).sqrt();
-        if (speed > speedLimit) {
-            self.dx = (self.dx / speed) * speedLimit;
-            self.dy = (self.dy / speed) * speedLimit;
+        let speed = (self.velocity.x * self.velocity.x + self.velocity.y * self.velocity.y).sqrt();
+        if speed > speed_limit {
+            self.velocity = (self.velocity / speed) * speed_limit;
         }
     }
 
     // Keep the boid in the window by stabilizing its velocity.
     // ± helps to stabilize it gradually. w/ an addition or soustraction
     // the transition is smooth to the opposite side with a short delay defined by the velocity.
-    // let dx = -1;
-    // if x < 200 { dx += 1 };
-    // >> x = 200 // dx = 0
-    // >> x = 200 // dx = 1
-    // Having this transition help for separation, alignment and cohesion near a border.
+    // >> let velocity: Vec2 = { x=-1.0, y=0.0 };
+    // >> if position < LEFT_BORDER { velocity.x += 1.0 };
+    // >> position = LEFT_BORDER // velocity.x = 0.0
+    // >> position = LEFT_BORDER // velocity.x = 1.0
+    // >> end
+    // Having this transition helps for separation, alignment and cohesion near a border.
     pub fn check_border(&mut self, window: Rect<f32>) {
-        let margin = 100.0;
+        let margin = MARGIN;
+        let turn_factor = 1.0;
 
-        if self.x < window.left() + margin {
-            self.dx += 1.0;
+        if self.position.x < window.left() + margin {
+            self.velocity.x += turn_factor;
         }
         
-        if self.x > window.right() - margin {
-            self.dx -= 1.0;
+        if self.position.x > window.right() - margin {
+            self.velocity.x -= turn_factor;
         }
 
-        if self.y < window.bottom() + margin {
-            self.dy += 1.0;
+        if self.position.y < window.bottom() + margin {
+            self.velocity.y += turn_factor;
         }
         
-        if self.y > window.top() - margin {
-            self.dy -= 1.0;
+        if self.position.y > window.top() - margin {
+            self.velocity.y -= turn_factor;
         }
     }
 
     // Keep a small distance away from other boids.
-    pub fn separation(&self, boids: &[Boid]) -> (f32, f32) {
-        let mut x = 0.0;
-        let mut y = 0.0;
+    pub fn separation(&self, boids: &[Boid]) -> Vec2 {
+        let mut velocity = Vec2::new(0.0, 0.0);
         let factor = 0.05; // Adjust the velocity of the action.
 
         for boid in boids.iter() {
             if boid != self {
-                if distance(&self, &boid) < 40.0 {
-                    x += self.x - boid.x;
-                    y += self.y - boid.y;
+                if distance(&self, &boid) < SEPARATION {
+                    velocity += self.position - boid.position;
                 }
             }
         }
 
-        x = x * factor;
-        y = y * factor;
+        velocity = velocity * factor;
 
-        (x, y)
+        velocity
     }
 
     // Match velocity with near boids.
-    pub fn alignment(&self, boids: &[Boid]) -> (f32, f32) {
-        let mut x = 0.0;
-        let mut y = 0.0;
+    pub fn alignment(&self, boids: &[Boid]) -> Vec2 {
+        let mut velocity = Vec2::new(0.0, 0.0);
         let mut neighbors = 0.0;
         let factor = 0.05; // Adjust the velocity of the action.
 
         for boid in boids.iter() {
             if boid != self {
-                if distance(&self, &boid) < 100.0 {
-                    x += boid.dx;
-                    y += boid.dy;
+                if distance(&self, &boid) < DISTANCE {
+                    velocity.x += boid.velocity.x;
+                    velocity.y += boid.velocity.y;
 
                     neighbors += 1.0;
                 }
@@ -114,26 +113,24 @@ impl Boid {
         }
 
         if neighbors > 0.0 {
-            x = ((x / neighbors) - self.dx) * factor;
-            y = ((y / neighbors) - self.dy) * factor;
+            velocity = ((velocity / neighbors) - self.velocity) * factor;
         }
 
-        (x, y)
+        velocity
     }
 
     // Fly towards the center of mass of neihbouring boids.
     // Get the average position of all the boids defined as the center of mass.
-    pub fn cohesion(&self, boids: &[Boid]) -> (f32, f32) {
-        let mut x = 0.0;
-        let mut y = 0.0;
+    pub fn cohesion(&self, boids: &[Boid]) -> Vec2 {
+        let mut velocity = Vec2::new(0.0, 0.0);
         let mut neighbors = 0.0;
         let factor = 0.005; // Adjust the velocity of the action.
 
         for boid in boids.iter() {
             if boid != self {
-                if distance(&self, &boid) < 100.0 {
-                    x += boid.x;
-                    y += boid.y;
+                if distance(&self, &boid) < DISTANCE {
+                    velocity.x += boid.position.x;
+                    velocity.y += boid.position.y;
 
                     neighbors += 1.0;
                 }
@@ -141,15 +138,14 @@ impl Boid {
         }
 
         if neighbors > 0.0 {
-            x = ((x / neighbors) - self.x ) * factor;
-            y = ((y / neighbors) - self.y ) * factor;
+            velocity = ((velocity / neighbors) - self.position) * factor;
         }
 
-        (x, y)
+        velocity
     }
 }
 
 // Pythagoras theorem a^2 + b^2 = c^2
 fn distance(boid1: &Boid, boid2: &Boid) -> f32 {
-    ((boid1.x - boid2.x) * (boid1.x - boid2.x) + (boid1.y - boid2.y) * (boid1.y - boid2.y)).sqrt()
+    ((boid1.position.x - boid2.position.x) * (boid1.position.x - boid2.position.x) + (boid1.position.y - boid2.position.y) * (boid1.position.y - boid2.position.y)).sqrt()
 }
